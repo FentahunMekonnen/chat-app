6a4349100464752b2135f4e9d83501e905068554
FentahunMekonnen <fentahunmekonnen@gmail.com>
2026-06-20 09:24:22 -0500

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import cors from "cors";
-
 import "dotenv/config";
+
+import { clerkMiddleware } from "@clerk/express";
+
+import User from "./models/user.model.js";
+import Message from "./models/message.model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-console.log(process.env.MONGO_URI)
-
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(clerkMiddleware());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get("/health", (req, res) => {
+  res.json({ message: "Hello from the API!" });
 });
 
 app.listen(PORT, () => {
+  connectDB();
+
   console.log(`Server is running on port ${PORT}`);
 });
